ccb7da62e508ee3e6134b19bf4ae8e816c711317
Armin Ahmadi <armin82004@gmail.com>
2025-10-19 18:39:49 +0330

    fix remove post color bug

--- app/(main)/components/deleteButton.tsx
+++ app/(main)/components/deleteButton.tsx
@@ -4,7 +4,7 @@ import { removePost } from "@/app/actions";
 export default function DeleteButton({ ID }: { ID: string }) {
   return (
     <button
-      className="mb-4 block text-sm bg-red-500 px-3 py-1 rounded-full cursor-pointer hover:bg-red-700 transition-colors border border-transparent active:border-red-500"
+      className="mb-4 block text-sm text-white bg-red-500 px-3 py-1 rounded-full cursor-pointer hover:bg-red-700 transition-colors border border-transparent active:border-red-500"
       onClick={() => removePost(ID)}
     >
       Remove Post
