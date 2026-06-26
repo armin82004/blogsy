fec087d5479a4d70966e28da63ac47d2f90bc5ee
Armin Ahmadi <armin82004@gmail.com>
2026-06-26 23:54:44 +0330

    adding supabse image url for fixing next image error

--- next.config.ts
+++ next.config.ts
@@ -9,7 +9,7 @@ const nextConfig: NextConfig = {
       },
       {
         protocol: "https",
-        hostname: "hqtedfmbeowgqudosubb.supabase.co",
+        hostname: "aqirxltxudaonqibhink.supabase.co",
       },
     ],
   },
